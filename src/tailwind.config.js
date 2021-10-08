fc5ddb5fbccf0dc8001c390b0482d841bf873846
ruie <ruiedelapena@gmail.com>
2021-10-08 13:27:19 +1100

    chore: delete .github and remove unnecessary comments inside tailwind config

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,9 +1,3 @@
-/**
- * Tailwind CSS configuration file
- *
- * docs: https://tailwindcss.com/docs/configuration
- * default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
- */
 const path = require("path");
 
 module.exports = {
